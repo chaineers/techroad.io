03e5b8742e19c17985e3f986b19d4b1bc3a9ea07
Harry Oh <harry@5004.pe.kr>
2018-07-18 17:37:44 +0900

    Add chain status

---
+++ custom_assets/js/script.js
@@ -0,0 +1,57 @@
+
+
+(function($) {
+
+  var insert_chain_status_html = function() {
+    var chain_status_html = `
+      <section class="mbr-section info1 chain_status">
+        <div class="container">
+          <div class="row justify-content-center content-row">
+
+            <div class="col-6 col-lg-3 col-md-6 text-center" >
+              <strong>Blocks</strong>
+              <p id="chain_blocks"></p>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6 text-center">
+              <strong>Difficulty</strong>
+              <p id="chain_difficulty"></p>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6 text-center">
+              <strong>Connections</strong>
+              <p id="chain_connections"></p>
+            </div>
+            <div class="col-6 col-lg-3 col-md-6 text-center">
+              <strong>Total</strong>
+              <p id="chain_total_amount"></p>
+            </div>
+
+          </div>
+
+          <div class="row justify-content-center content-row">
+            <div class="col-12 text-right" id="now_time">
+            </div>
+          </div>
+
+        </div>
+      </section>
+    `;
+    $("section.chain_status").remove();
+    $("section#footer1-2").before(chain_status_html);
+  }
+
+  var fill_chain_summary = function() {
+    $.get("https://a7exuhkake.execute-api.ap-northeast-2.amazonaws.com/prod/summary",
+      function(data) {
+        insert_chain_status_html();
+        $(".chain_status #chain_blocks").text(data.blocks);
+        $(".chain_status #chain_difficulty").text(data.difficulty);
+        $(".chain_status #chain_connections").text(data.connections);
+        $(".chain_status #chain_total_amount").text(data.total_amount);
+        $(".chain_status #now_time").text(new Date().toLocaleString());
+        setTimeout(fill_chain_summary, 60000);
+      });
+  }
+
+  fill_chain_summary();
+
+})(jQuery);
